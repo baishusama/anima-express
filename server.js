73b6aefe08bc2f98ae18f0e0f514733b9f07dd96
baishusama <baishusama@outlook.com>
2017-12-13 00:42:47 +0800

    re-initialized server.js

--- server.js
+++ server.js
@@ -1,46 +1,69 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// Get API routes
+const api = require('./server/routes/api');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
+// Uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
+// Parsers for POST data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(cookieParser());
+// Point static path to dist/
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/', index);
-app.use('/users', users);
+// Set api routes
+app.use('/api', api);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Caught all other routes and return the index file
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+/**
+ * Get port from environment
+ */
+const port = process.env.PORT || 3000;
+app.set('port', port);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+/**
+ * Create HTTP server
+ */
+// const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces
+ */
+app.listen(port, () => {
+  console.log(`API running on localhost:${port}`);
 });
 
-module.exports = app;
+/*// catch 404 and forward to error handler
+ app.use(function(req, res, next) {
+ var err = new Error('Not Found');
+ err.status = 404;
+ next(err);
+ });
+
+ // error handler
+ app.use(function(err, req, res, next) {
+ // set locals, only providing error in development
+ res.locals.message = err.message;
+ res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+ // render the error page
+ res.status(err.status || 500);
+ res.render('error');
+ });
+
+ module.exports = app;*/
